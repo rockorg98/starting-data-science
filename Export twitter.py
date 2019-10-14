d297d087242591b91b0fdf3ac4ee726b31b5fdb6
rockorg98 <42319748+rockorg98@users.noreply.github.com>
2019-10-14 23:37:03 +0530

    Update Export twitter.py

--- Export twitter.py
+++ Export twitter.py
@@ -3,11 +3,11 @@ import tweepy
 from textblob import TextBlob
 import csv
 
-consumer_key='KH9P3w4ClSzk3OUmadYQ28DU8'
-consumer_secret='d9E26bpZsmf78z6ldi5aqWhZHZAQbHH5MgRB2IjMzGdpkdnhHE'
+consumer_key='xxxx'
+consumer_secret='yyyyy'
 
-access_token='1175651094627672064-LRWcIsjQKFp4x8yW5QayxBmmrHnTlr'
-access_token_secret='mXnx24Q6T3YgxrH2x5wTv5ToN5qR4pCakFML3Af5l7rDm'
+access_token='lllllll'
+access_token_secret='mmmmmm'
 
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
